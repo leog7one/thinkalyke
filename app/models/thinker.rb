ba1d1956ac83251ffc32a1ea7baffbab78d43776
leog7one <lguzman@me.com>
2017-10-20 11:57:26 -0500

    added devise for authentication

---
+++ app/models/thinker.rb
@@ -0,0 +1,6 @@
+class Thinker < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
