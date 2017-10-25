85550f0c0d00f6a1bbe78390f671a4657dec6e2d
leog7one <lguzman@me.com>
2017-10-25 00:27:41 -0500

    allow unregister from thought

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,9 +1,8 @@
 class AttendancesController < ApplicationController
 	before_action :authenticate_thinker!
+	before_action :set_thought
 
 	def create
-		@thought = Thought.find(params[:thought_id])
-
 		@attendance = @thought.attendances.where(thinker: current_thinker).first_or_create
 
 		if @attendance.save
@@ -14,4 +13,20 @@ class AttendancesController < ApplicationController
 			render 'thoughts/show'
 		end
 	end
+
+	def destroy
+		@attendance = @thought.attendances(params[:id])
+
+		@attendance.delete_all
+			flash[:alert] = "You have been unregistered from this Thought"
+			redirect_to thinker_path(current_thinker)
+	end
+
+
+	private
+
+	def set_thought
+		@thought = Thought.find(params[:thought_id])
+	end
+
 end
