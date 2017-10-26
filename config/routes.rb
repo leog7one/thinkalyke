04c8f593524e535cb6b7d4b4ddf20f8fe9727ba8
leog7one <lguzman@me.com>
2017-10-25 20:01:40 -0500

    add tagging to thoughts

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   resources :thinkers, only: [:show]
   resources :categories, only: [:show]
+  resources :tags, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025030135) do
+ActiveRecord::Schema.define(version: 20171025180004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 20171025030135) do
   add_index "comments", ["thinker_id"], name: "index_comments_on_thinker_id", using: :btree
   add_index "comments", ["thought_id"], name: "index_comments_on_thought_id", using: :btree
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "thinkers", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -66,6 +72,16 @@ ActiveRecord::Schema.define(version: 20171025030135) do
   add_index "thinkers", ["email"], name: "index_thinkers_on_email", unique: true, using: :btree
   add_index "thinkers", ["reset_password_token"], name: "index_thinkers_on_reset_password_token", unique: true, using: :btree
 
+  create_table "thought_tags", force: :cascade do |t|
+    t.integer  "thought_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "thought_tags", ["tag_id"], name: "index_thought_tags_on_tag_id", using: :btree
+  add_index "thought_tags", ["thought_id"], name: "index_thought_tags_on_thought_id", using: :btree
+
   create_table "thoughts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -86,5 +102,7 @@ ActiveRecord::Schema.define(version: 20171025030135) do
   add_foreign_key "attendances", "thoughts"
   add_foreign_key "comments", "thinkers"
   add_foreign_key "comments", "thoughts"
+  add_foreign_key "thought_tags", "tags"
+  add_foreign_key "thought_tags", "thoughts"
   add_foreign_key "thoughts", "thinkers"
 end
