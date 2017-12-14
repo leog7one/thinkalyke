7888e4eeac328868b95855e4bef4e244e28f5ef5
leog7one <lguzman@me.com>
2017-12-14 12:13:49 -0600

    started ajax for comment by blocking regular comment submit

---
+++ app/assets/javascripts/admin/thoughts.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#new_comment').on('submit', function(e) {
+        alert('You clicked submit!')
+        e.preventDefault();
+
+        //need url to submit post request
+        //need the form data
+    })
+})
